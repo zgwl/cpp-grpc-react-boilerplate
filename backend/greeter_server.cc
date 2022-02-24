9ddc1c6e03e8335c32c099b5d5e1209f0589b94c
Liang Xu <lxu1@flexport.com>
2022-02-23 16:55:58 -0800

    split the backend and frontend, and create an empty frontend app

--- backend/greeter_server.cc
+++ backend/greeter_server.cc
@@ -1,22 +1,18 @@
+#include <grpcpp/grpcpp.h>
+
 #include <iostream>
 #include <memory>
 #include <string>
 
-#include <grpcpp/grpcpp.h>
-
-#ifdef BAZEL_BUILD
 #include "protos/helloworld.grpc.pb.h"
-#else
-#include "helloworld.grpc.pb.h"
-#endif
 
 using grpc::Server;
 using grpc::ServerBuilder;
 using grpc::ServerContext;
 using grpc::Status;
-using helloworld::HelloRequest;
-using helloworld::HelloReply;
 using helloworld::Greeter;
+using helloworld::HelloReply;
+using helloworld::HelloRequest;
 
 // Logic and data behind the server's behavior.
 class GreeterServiceImpl final : public Greeter::Service {
